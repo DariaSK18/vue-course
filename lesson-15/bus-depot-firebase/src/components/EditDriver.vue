0b340b25f6c6c824df4e598a5906b6f222507145
Daria Steblovska <darias1896.96@gmail.com>
2025-09-10 01:54:18 +0100

    driver form edit with vee-validate and yup

--- lesson-15/bus-depot-firebase/src/components/EditDriver.vue
+++ lesson-15/bus-depot-firebase/src/components/EditDriver.vue
@@ -1,42 +1,53 @@
 <template>
+  <h3>Edit Info :</h3>
   <div>
-    <h3>Edit Info :</h3>
-    <div class="box">
-      <label for="name">Driver's name </label>
-      <input
-        id="name"
-        type="text"
-        autocomplete="off"
-        v-model="driverData.name"
-      />
-    </div>
-    <div class="box">
-      <label for="experience">Driver's experience </label>
-      <input
-        id="experience"
-        type="number"
-        min="0"
-        autocomplete="off"
-        v-model="driverData.experience"
-      />
-    </div>
-  </div>
-  <div class="box_button">
-    <button @click="onSave()">Save</button>
-    <button @click="onCancel()">Cancel</button>
+    <Form
+      :key="driverId"
+      :validation-schema="schema"
+      :initial-values="driverData"
+      @submit="onSubmit"
+    >
+      <div class="box">
+        <label for="name">Driver's name </label>
+        <Field name="name" type="text" id="name" autocomplete="off" />
+        <ErrorMessage name="name" />
+      </div>
+      <div class="box">
+        <label for="experience">Driver's experience </label>
+        <Field name="experience" type="number" autocomplete="off" />
+        <ErrorMessage name="experience" />
+      </div>
+      <div class="box_button">
+        <button type="submit">Save</button>
+        <button type="button" @click="onCancel">Cancel</button>
+      </div>
+    </Form>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
+import { Form, Field, ErrorMessage } from "vee-validate";
+import * as yup from "yup";
+
 export default {
   name: "EditDriver",
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
   data() {
     return {
       driverData: {
         name: "",
-        experience: 0,
+        experience: null,
       },
+      schema: yup.object({
+        name: yup.string().required(),
+        experience: yup.number().required().positive().max(80),
+        // .min(0.1)
+      }),
     };
   },
   computed: {
@@ -50,11 +61,21 @@ export default {
     onCancel() {
       this.$router.back();
     },
-    onSave() {
-      if (!this.driverId) {
-        this.addItem(this.driverData);
-      } else this.updateItem({ itemId: this.driverId, data: this.driverData });
-      this.$router.push({ name: "drivers" });
+    async onSubmit(value, { resetForm }) {
+      try {
+        if (this.driverId) {
+          await this.updateItem({
+            itemId: this.driverId,
+            data: value,
+          });
+        } else {
+          await this.addItem(value);
+        }
+        resetForm();
+        this.$router.push({ name: "drivers" });
+      } catch {
+        console.error(error);
+      }
     },
   },
   created() {
@@ -62,5 +83,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped></style>
