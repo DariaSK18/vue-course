0e38cfe41b6e40fe68407805ba5bae80c9ec6175
Daria Steblovska <darias1896.96@gmail.com>
2025-09-13 18:12:33 +0100

    components added to views

--- lesson-17/notes-app/src/views/FriendsView.vue
+++ lesson-17/notes-app/src/views/FriendsView.vue
@@ -1,13 +1,14 @@
 <template>
-    <div>
-Friends
-    </div>
+  <div>
+    <h2>Friends List</h2>
+    <list-item />
+    <add-item><template #default>New Friend </template></add-item>
+  </div>
 </template>
 
 <script setup>
-
+import ListItem from '../components/ListItem.vue'
+import AddItem from '../components/AddItem.vue'
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- lesson-17/notes-app/src/views/GiftsView.vue
+++ lesson-17/notes-app/src/views/GiftsView.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
-Gifts
+<h2>Gifts List</h2>
+<list-item/>
+<add-item><template #default>New Gift </template></add-item>
     </div>
 </template>
 
 <script setup>
-
+import ListItem from "../components/ListItem.vue";
+import AddItem from "../components/AddItem.vue";
 </script>
 
 <style lang="scss" scoped>
