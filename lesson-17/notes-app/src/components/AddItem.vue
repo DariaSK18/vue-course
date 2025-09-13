590e69741742f08d99f8483a6625ff3530ca0135
Daria Steblovska <darias1896.96@gmail.com>
2025-09-13 17:56:55 +0100

    create add itrm amd list item components

---
+++ lesson-17/notes-app/src/components/AddItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+<label>
+    <slot></slot>
+    <input type="text" autocomplete="off">
+    <button>Add</button>
+</label>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ lesson-17/notes-app/src/components/ListItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="list-item">
+        <span>Title</span>
+        <button>Delete</button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
